b4c763c56a7d7ecc51087dba2d0dddf5ff01aafe
Marcus Jensen <marcus@eliteeleverne.dk>
2021-01-04 11:36:40 +0100

    models

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -1,11 +1,124 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+using System;
 
 namespace HUS_project.Models
 {
     public class Booking
     {
+        #region Fields
+        private User customer;
+        private User deliveredBy;
+        private User returnedBy;
+        private List<Device> devices;
+        private List<Device> logs;
+        private string building;
+        private string roomNumber;
+        private DateTime dateBorrowed;
+        private DateTime dateReturned;
+        #endregion
+
+        #region Properties
+        public User Customer
+        {
+            get
+            {
+                return customer;
+            }
+            set
+            {
+                customer = value;
+            }
+        }
+        public User DeliveredBy
+        {
+            get
+            {
+                return deliveredBy;
+            }
+            set
+            {
+                deliveredBy = value;
+            }
+        }
+        public User ReturnedBy
+        {
+            get
+            {
+                return returnedBy;
+            }
+            set
+            {
+                returnedBy = value;
+            }
+        }
+        public List<Device> Devices
+        {
+            get
+            {
+                return devices;
+            }
+            set
+            {
+                devices = value;
+            }
+        }
+        public List<Device> Logs
+        {
+            get
+            {
+                return logs;
+            }
+            set
+            {
+                logs = value;
+            }
+        }
+        public string Building
+        {
+            get
+            {
+                return building;
+            }
+            set
+            {
+                building = value;
+            }
+        }
+        public string RoomNumber
+        {
+            get
+            {
+                return roomNumber;
+            }
+            set
+            {
+                roomNumber = value;
+            }
+        }
+        public DateTime DateBorrowed
+        {
+            get
+            {
+                return dateBorrowed;
+            }
+            set
+            {
+                dateBorrowed = value;
+            }
+        }
+        public DateTime DateReturned
+        {
+            get
+            {
+                return dateReturned;
+            }
+            set
+            {
+                dateReturned = value;
+            }
+        }
+        #endregion
     }
 }
--- Models/Device.cs
+++ Models/Device.cs
@@ -1,11 +1,76 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using System.Linq;
+using System;
 
 namespace HUS_project.Models
 {
     public class Device
     {
+        #region Fields
+        private int id;
+        private string description;
+        private string category;
+        private string room;
+        private List<string> notes;
+        #endregion
+
+        #region Properties
+        public int ID
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+        public string Category
+        {
+            get
+            {
+                return category;
+            }
+            set
+            {
+                category = value;
+            }
+        }
+        public string Room
+        {
+            get
+            {
+                return room;
+            }
+            set
+            {
+                room = value;
+            }
+        }
+        public List<string> Notes
+        {
+            get
+            {
+                return notes;
+            }
+            set
+            {
+                notes = value;
+            }
+        }
+        #endregion
     }
 }
