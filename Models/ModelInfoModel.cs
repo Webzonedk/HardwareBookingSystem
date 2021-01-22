8904f5c35d6ff1583c63a1bd669614163a648b42
Kent Pedersen <webzone@kpdesign.dk>
2021-01-22 09:32:12 +0100

    Added Model ModelInfoModel

---
+++ Models/ModelInfoModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HUS_project.Models
+{
+    public class ModelInfoModel
+    {
+        #region fields
+        private DeviceModel device;
+        private List<DeviceModel> borrowedDevices;
+        #endregion
+
+
+
+
+        #region Properties
+
+        public DeviceModel Device
+        {
+            get { return device; }
+            set { device = value; }
+        }
+
+
+        public List<DeviceModel> BorrowedDevices
+        {
+            get { return borrowedDevices; }
+            set { borrowedDevices = value; }
+        }
+     
+        #endregion
+
+
+
+        #region Constructors
+        public ModelInfoModel()
+        {
+
+        }
+
+        public ModelInfoModel(DeviceModel device, List<DeviceModel> borrowedDevices)
+        {
+            this.device = device;
+            this.borrowedDevices = borrowedDevices;
+        }
+        #endregion
+    }
+}
