0b4d1012cb9768083f6840eb9c2fd5bdb3ac959f
AndreasBollNielsen <64074621+AndreasBollNielsen@users.noreply.github.com>
2021-02-18 11:18:54 +0100

    added json for andreas

--- Controllers/DeviceController.cs
+++ Controllers/DeviceController.cs
@@ -6,17 +6,28 @@ using System.Threading.Tasks;
 using System.Diagnostics;
 using HUS_project.Models;
 using HUS_project.DAL;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Http;
 
 namespace HUS_project.Controllers
 {
     public class DeviceController : Controller
     {
+        private readonly IConfiguration configuration;
 
-
-        public IActionResult CreateDevice()
+        // constructor of homecontroller
+        public DeviceController(IConfiguration config)
         {
-            return View();
+            this.configuration = config;
+        }
+
+        public IActionResult CreateDevice(DeviceModel deviceData)
+        {
+            //generate an instance of the database manager
+            DBManagerDevice DBDevice = new DBManagerDevice(configuration);
+            //send data to the manager
+
+            return View("EditDevice");
         }
 
 
