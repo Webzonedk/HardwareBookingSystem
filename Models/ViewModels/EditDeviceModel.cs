6b92f11b6d79f08e52e2d9b115a780e699b11e99
AndreasBollNielsen <64074621+AndreasBollNielsen@users.noreply.github.com>
2021-02-25 15:04:01 +0100

    Added viewmodels to ViewModel folder

--- Models/ViewModels/EditDeviceModel.cs
+++ Models/ViewModels/EditDeviceModel.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace HUS_project.Models
+namespace HUS_project.Models.ViewModels
 {
     public class EditDeviceModel
     {
