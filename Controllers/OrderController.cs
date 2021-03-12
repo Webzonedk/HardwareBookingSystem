72b61f25f5c38e3836b4e5b4170f3b1316b0adcd
Thomas Carl Alexander Dänhardt <thom09r7@zbc.dk>
2021-03-12 12:51:24 +0100

    P. 11 + ?? Lined up

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Diagnostics;
 using HUS_project.Models;
+using HUS_project.Models.ViewModels;
 using HUS_project.DAL;
 using Microsoft.AspNetCore.Http;
 
@@ -18,13 +19,6 @@ namespace HUS_project.Controllers
             return View();
         }
 
-        public IActionResult SeeBooking(string bookingID)
-        {
-            BookingModel booking = new BookingModel();
-            booking.BookingID = Convert.ToInt32(bookingID);
-            return View("ExecuteOrder", booking);
-        }
-
         public IActionResult ReturnDevices()
         {
             return View();
@@ -35,5 +29,17 @@ namespace HUS_project.Controllers
             return View();
         }
 
+        public IActionResult SeeBooking(string bookingID)
+        {
+            BookingModel booking = new BookingModel();
+            booking.BookingID = Convert.ToInt32(bookingID);
+
+            ExecuteOrderModel executeOrderModel = new ExecuteOrderModel(
+                booking,
+                new List<ItemLineModel>(),
+                new Dictionary<ItemLineModel, StorageLocationModel>()
+                );
+            return View("ExecuteOrder", executeOrderModel);
+        }
     }
 }
---
+++ Models/ViewModels/ExecuteOrderModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HUS_project.Models.ViewModels
+{
+    public class ExecuteOrderModel
+    {
+        #region Variables
+            private BookingModel booking;
+            private List<ItemLineModel> modelsInStorage;
+            private Dictionary<ItemLineModel, StorageLocationModel> storageLocations;
+        #endregion
+        #region GetsNSets
+            public BookingModel Booking
+            {
+                get { return booking; }
+                set { booking = value; }
+            }
+
+            public List<ItemLineModel> ModelsInStorage
+            {
+                get { return modelsInStorage; }
+                set { modelsInStorage = value; }
+            }
+
+            public Dictionary<ItemLineModel, StorageLocationModel> StorageLocations
+            {
+                get { return storageLocations; }
+                set { this.storageLocations = value; }
+            }
+        #endregion
+        #region Constructors
+            public ExecuteOrderModel()
+            {
+
+            }
+            public ExecuteOrderModel(BookingModel Booking, List<ItemLineModel> ModelsInStorage, Dictionary<ItemLineModel, StorageLocationModel> StorageLocations)
+            {
+                this.booking = Booking;
+                this.modelsInStorage = ModelsInStorage;
+                this.storageLocations = StorageLocations;
+            }
+        #endregion
+    }
+}
