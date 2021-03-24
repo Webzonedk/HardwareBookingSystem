f290998cb30c6fe0114570dc4efb3190a0f4501f
Kent Pedersen <webzone@kpdesign.dk>
2021-03-24 10:42:09 +0100

    added and outcommented sorting JS

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -18,9 +18,9 @@ function submit(input) {
 
 }
 
-//Used to sort the columns in the views
+////Used to sort the columns in the views
 
-    function sorting() {
-        document.getElementById('ChosenProvider').value = '1';
-        document.getElementById('loginForm').submit();
-    }
+//    function sorting() {
+//        document.getElementById('ChosenProvider').value = '1';
+//        document.getElementById('loginForm').submit();
+//    }
