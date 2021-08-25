ebc445e0a19d9defbc3f98763f6475f8e58cbde3
AndreasBollNielsen <64074621+AndreasBollNielsen@users.noreply.github.com>
2021-08-25 15:42:32 +0200

    cleaned up site.js

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -5,48 +5,4 @@
 
 // Write your JavaScript code.
 
-function SubmitValidation(input) {
-
-    document.getElementById(input).submit();
-
-}
-
-
-
-function CheckInputField() {
-
-    var field = document.getElementById("deleteNote").value;
-    if (field == "") {
-
-        /* var AlertBox = confirm("For at slette enheden, skal årsag udfyldes");*/
-        document.getElementById("warning").innerText = "For at slette enheden, skal årsag udfyldes";
-        //if (AlertBox == true || AlertBox == false) {
-        //}
-        return false;
-    }
-    else {
-
-        document.getElementById("editForm").action = "DeleteDevice";
-        document.getElementById("warning").innerText = '';
-        console.info("delete");
-        form.submit();
-    }
-}
-
-
-
-
-//call submit at scanDevice view
-function ReturnToEdit(val) {
-    //var location = $("#content").val();
-    //console.log(location);
-    //var id = 0;
-
-    //var output = location + '-' + id;
-    //console.log(output);
-
-    //submit();
-    console.log("test test test" + val);
-}
-
 
