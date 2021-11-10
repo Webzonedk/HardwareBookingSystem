63e118f07a1ddcf4259221e76faf06fc9eab7743
Thomas Carl Alexander Dänhardt <thom09r7@zbc.dk>
2021-11-10 16:02:10 +0100

    MyBookings page added

---
+++ Models/ViewModels/MyBookingsModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HUS_project.Models.ViewModels
+{
+    public class MyBookingsModel
+    {
+        List<BookingModel> activeBookings;
+        List<BookingModel> comingBookings;
+        List<BookingModel> oldBookings;
+        public List<BookingModel> ActiveBookings
+        {
+            get { return this.activeBookings; }
+            set { this.activeBookings = value; }
+        }
+        public List<BookingModel> ComingBookings
+        {
+            get { return this.comingBookings; }
+            set { this.comingBookings = value; }
+        }
+        public List<BookingModel> OldBookings
+        {
+            get { return this.oldBookings; }
+            set { this.oldBookings = value; }
+        }
+        public MyBookingsModel()
+        {
+            
+        }
+    }
+}
